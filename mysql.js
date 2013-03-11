08438110879576046de7554e005b21b9657cc646
liubiao <liubiao4123@126.com>
2013-03-11 18:49:04 +0800

    update

--- mysql.js
+++ mysql.js
@@ -1,44 +1,61 @@
 var mysql_require = require('mysql');
 
-var mysql = {};
+var mysql = {
+		ktpd:{},
+		sssg:{remote:1,url:'https://sg.liubioa.com/websocket/get_new_income',},
+		js:{},
+};
 
-//mysql数据库连接状态，0表示未连接，1表示已连接
-mysql.status = 0;
-
-//mysql连接
-mysql.connect = function(){
-	mysql.connection = mysql_require.createConnection({
-		host: 'houtai.mysql.ktpd.xd.com',
-		user: 'ktpd',
-		password: 'mNSzAjXJpS33Tuen',
+mysql.ktpd.config = {
+		host: '127.0.0.1',
+		user: 'root',
+		password: 'xindong',
 		database: 'ktpd_houtai',
-	});
+};
+
+mysql.js.config = {
+		host: '127.0.0.1',
+		user: 'root',
+		password: 'xindong',
+		database: 'js_houtai',
+};
+
+for(var game in mysql )
+{
+	//mysql数据库连接状态，0表示未连接，1表示已连接
+	mysql[game].status = 0;
 	
-	mysql.connection.connect(function(err){
-		if (err){
-			console.log("mysql connection error: "+err);
-			mysql.status = 0;
-		}
-		else{
-			console.log("mysql connect successful!");
-			mysql.status = 1;
-		}
-	});
+	if (mysql[game].remote) continue;
 	
-	mysql.connection.on('error', function(err){
-		console.log("mysql connection error: "+ err);
-		mysql.status = 0;
-		mysql.connect();
-	});
+	//mysql连接
+	mysql[game].connect = function(the_game){
+		mysql[the_game].connection = mysql_require.createConnection(mysql[the_game].config);
+		
+		mysql[the_game].connection.connect(function(err){
+			if (err){
+				console.log(the_game+"  mysql connection error: "+err);
+				mysql[the_game].status = 0;
+			}
+			else{
+				console.log(the_game+"  mysql connect successful!");
+				mysql[the_game].status = 1;
+			}
+		});
+		
+		mysql[the_game].connection.on('error', function(err){
+			console.log(the_game+"  mysql connection error: "+ err);
+			mysql[the_game].status = 0;
+			mysql[the_game].connect();
+		});
 
+	}
+
+	//mysql关闭
+	mysql[game].close = function(the_game){
+		mysql[the_game].connection.destroy();
+		mysql[the_game].status = 0;
+		console.log(the_game+"  mysql connection closed!");
+	}
 }
-
-//mysql关闭
-mysql.close = function(){
-	mysql.connection.destroy();
-	mysql.status = 0;
-	console.log("mysql connection closed!");
-}
-
 
 exports.mysql = mysql;
--- websocket.js
+++ websocket.js
@@ -1,20 +1,32 @@
+var nodegrass = require('nodegrass');
+
+var md5 = require('./md5');
+md5 = md5.md5;
 
 var mysql_require = require('./mysql');
 
 var mysql = mysql_require.mysql;
 
 //存储最新充值记录，按平台分开存储
-var new_income = new Object();
+var new_income = {};
+
+//当前的client连接数
+var connection_num = {};
+
+//最大的order_id
+var max_id = {};
+
+for(var game in mysql)
+{
+	new_income[game] = new Object();
+	connection_num[game] = 0;
+	max_id[game] = 0;
+}
 
 var new_income_channel_num = 50;
 
-var max_id = 0;
-
-//当前的client连接数
-var connection_num = 0;
-
 //get_new_income函数执行间隔，单位：秒
-var time_distance = 5;
+var time_distance = 10;
 
 
 //启动websocket服务器
@@ -34,36 +46,64 @@ function socket_start()
 	});
 	server.listen(8880);
 	var socket_io = require('socket.io').listen(server);
+	
+	//关闭调试信息
+	socket_io.set('log level', 1); 
 
 	//监听连接事件
 	socket_io.sockets.on('connection', function (socket) {
-		connection_num++;
-		socket.on('new_income',function(data){ 
-			var channel_ids = data.channel_ids;
-			var id = data.id?data.id:0;
-			
-			var return_income = new Object();
-			
-			for( var i in new_income)
-			{
-				if( channel_ids.length && !in_array(i,channel_ids)) continue;
-				
-				for( var j in new_income[i] )
-				{
-					if ( new_income[i][j].id <= id ) break;
-					return_income[new_income[i][j].id] = new_income[i][j];
-				}
-			}
-			
-			var return_income_sort = object_sort(return_income);
-			
-			socket.emit('new_income',return_income_sort);
-		});
+		var query = socket.handshake.query;
 		
-		//断开连接
-		socket.on("disconnect",function(){
-			connection_num--;
-		});
+		if ( query.game && mysql[query.game] )
+		{
+			//var sign = md5(string);
+			var sign = 1;
+			//验证签名
+			if ( query.sign && query.sign == sign )
+			{
+				connection_num[query.game]++;
+				socket.on('new_income',function(data){ 
+					var channel_ids = data.channel_ids;
+					var id = data.id?data.id:0;
+					
+					var return_income = new Object();
+					
+					for( var i in new_income[query.game])
+					{
+						if( channel_ids.length && !in_array(i,channel_ids)) continue;
+						
+						for( var j in new_income[query.game][i] )
+						{
+							if ( new_income[query.game][i][j].id <= id ) break;
+							return_income[new_income[query.game][i][j].id] = new_income[query.game][i][j];
+						}
+					}
+					
+					var return_income_sort = object_sort(return_income);
+					
+					if ( !is_object_empty( return_income_sort ) )
+					{
+						console.log(query.game+' client:');
+						console.log(return_income_sort);
+					}
+					
+					socket.emit('new_income',return_income_sort);
+				});
+				
+				//断开连接
+				socket.on("disconnect",function(){
+					connection_num[query.game]--;
+				});
+			}
+			else
+			{
+				console.log("Error: sign验证失败");
+			}
+		}
+		else
+		{
+			
+		}
 	});
 }
 
@@ -71,38 +111,79 @@ function socket_start()
 //获取最新的充值信息
 function get_new_income()
 {
-	console.log("当前的连接数："+connection_num);
-	//没有连接时不查询数据库
-	if (connection_num)
+	var get_new_income_by_game = function(game)
 	{
-		if ( !mysql.status )
-		{
-			mysql.connect();
-		}
+		console.log(game+",当前的连接数："+connection_num[game]);
 		
-		//防止mysql连接出错
-		if( mysql.status )
+		//没有连接时不查询数据库
+		if (connection_num[game])
 		{
-			var sql = 'select a.id,a.channel_id,a.server_id,a.player_name,a.gold,a.create_time,b.name as server_name,c.chinese_name from orders as a left join servers as b on a.server_id=b.id left join channels as c on a.channel_id=c.id where a.id > '+ max_id +' order by a.id desc limit 100';
-			mysql.connection.query(sql,function (err, rows, fields){
-				if (err){
-					mysql.close();
+			if ( mysql[game].remote )
+			{
+				var time = parseInt(Date.parse(new Date())/1000);
+				var string = '';
+				var url = mysql[game].url+'?max_id='+max_id[game]+'&time='+time+'&sign='+md5(string);
+				
+				nodegrass.get(url,function(data,status,headers){
+					var income = eval("("+data+")");
+					if (status === 200)
+					{
+						set_new_income(income,game);
+					}
+					else
+					{
+						console.log('Error: '+status+', info: '+income);
+					}
+				},'utf8').on('error',function(e){
+					console.log('Error: get '+ game +' url error, '+e.message);
+				});
+			}
+			else
+			{
+				if ( !mysql[game].status )
+				{
+					mysql[game].connect(game);
 				}
-				else{
-					set_new_income(rows);
+				
+				//防止mysql连接出错
+				if( mysql[game].status )
+				{
+					var sql = 'select * from orders  where id >'+max_id[game];
+					
+					mysql[game].connection.query(sql,function (err, rows, fields){
+						if (err){
+							mysql[game].close(game);
+						}
+						else{
+							set_new_income(rows,game);
+						}
+					});
 				}
-			});
+			}
+			
+		}
+		else
+		{
+			new_income[game] = new Object();
+			max_id[game] = 0;
+			
+			if ( !mysql[game].remote && mysql[game].status)
+			{
+				mysql[game].close(game);
+			}
 		}
 	}
-	else
+	
+	for( var game in mysql )
 	{
-		new_income = new Object();
-		max_id = 0;
-		if ( mysql.status )
-		{
-			mysql.close();
-		}
+		//get_new_income_by_game(game);
+		//下面是调试的代码，线上请将下面几行注释，上面一行反注释
+		var time = parseInt(Date.parse(new Date())/1000);
+		var rows = [{id:time,channel_id:1,content:'now time is: '+time}];
+		set_new_income(rows,game);
 	}
+	console.log(' ');
+	
 	setTimeout(get_new_income,time_distance*1000);
 }
 
@@ -110,26 +191,31 @@ get_new_income();
 
 
 //获取到的最新充值存储到new_income对象中
-function set_new_income(income)
+function set_new_income(income,game)
 {
+	if(!game)
+	{
+		console.log('Error:game为空');
+		return;
+	}
 	if( income )
 	{
 		for(var i=income.length-1;i>=0;i--)
 		{
-			max_id = income[i].id;
-			var channel_id = income[i]['channel_id'];
+			max_id[game] = income[i].id;
+			var channel_id = income[i].channel_id;
 			if (!channel_id) continue;
 			
-			if ( typeof new_income[channel_id] !== 'object') new_income[channel_id] = new Array();
+			if ( typeof new_income[game][channel_id] !== 'object') new_income[game][channel_id] = new Array();
 			
-			for( var k=new_income[channel_id].length-1;k>=0;k-- )
+			for( var k=new_income[game][channel_id].length-1;k>=0;k-- )
 			{
-				new_income[channel_id][k+1] = new_income[channel_id][k];
+				new_income[game][channel_id][k+1] = new_income[game][channel_id][k];
 			}
-			new_income[channel_id][0] = income[i];
-			if ( new_income[channel_id].length > new_income_channel_num )
+			new_income[game][channel_id][0] = income[i];
+			if ( new_income[game][channel_id].length > new_income_channel_num )
 			{
-				new_income[channel_id].splice(new_income_channel_num,new_income[channel_id].length-new_income_channel_num);
+				new_income[game][channel_id].splice(new_income_channel_num,new_income[game][channel_id].length-new_income_channel_num);
 			}
 		}
 	}
@@ -173,6 +259,15 @@ function object_sort(data)
 		}
 	}
 	return result;
+}
+
+function is_object_empty(obj)
+{
+	for (var i in obj)
+	{
+		return false;
+	}
+	return true;
 }
 
 exports.start = socket_start;
